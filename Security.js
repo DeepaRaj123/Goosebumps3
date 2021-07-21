7c64bf70e948d5864df5612b5c430518da5823e1
DeepaRaj123 <36413136+DeepaRaj123@users.noreply.github.com>
2021-07-21 15:35:36 +0530

    Update Security.js

--- Security.js
+++ Security.js
@@ -3,32 +3,42 @@ class Security {
     constructor(){
  
         this.access1 = createInput("Code1")
-        this.access1.position(100,90);
-        this.access1.style('background', 'white');  
-
+       
         this.button1 = createButton('Check');
-        this.button1.position(100,120);
-        this.button1.style('background', 'lightgrey');    
-
+       
         this.access2 = createInput("Code2")
-        this.access2.position(700,190);
-        this.access2.style('background', 'white');  
-
+       
         this.button2 = createButton('Check');
-        this.button2.position(700,220);
-        this.button2.style('background', 'lightgrey');
-
+       
         this.access3 = createInput("Code3")
-        this.access3.position(100,290);
-        this.access3.style('background', 'white');  
-
+        
         this.button3 = createButton('Check');
-        this.button3.position(100,320);
-        this.button3.style('background', 'lightgrey'); 
+        this.score =0;
+       }
+    hide() {
+        security.button1.hide();
+    this.access1.hide();
+    this.button2.hide();
+    this.access2.hide();
+    this.button3.hide();
+    this.access3.hide();  
     }
 
     display(){
-        this.score=0;
+
+        this.access1.position(100,90);
+        this.access1.style('background', 'white');  
+        this.button1.position(100,120);
+        this.button1.style('background', 'lightgrey');    
+        this.access2.position(700,190);
+        this.access2.style('background', 'white');  
+        this.button2.position(700,220);
+        this.button2.style('background', 'lightgrey');
+        this.access3.position(100,290);
+        this.access3.style('background', 'white');  
+        this.button3.position(100,320);
+        this.button3.style('background', 'lightgrey'); 
+  
         this.button1.mousePressed(() => {
             if(security.authenticate(accessCode1,this.access1.value())){
                 this.button1.hide();
@@ -48,9 +58,7 @@ class Security {
                 this.score++;
 
                 console.log(this.score)
-                // textSize(20);
-                // fill("white");
-                // text("Score: " +this.score, 450, 50);
+               
             }
         });
 
@@ -69,11 +77,12 @@ class Security {
                     fill("black")
                     textSize(40);
                     text("TREASURE UNLOCKED",250, 200);
-                    home2();
-                  }
+
+                    gameState = level1win;
+                }
                   else{
-                      losehome2();
-                  }
+                    gameState = level1end;
+                }
             }
         });
         
